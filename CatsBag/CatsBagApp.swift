a54780802a9d00e7434c0e09c205373dd01bc9d3
Brado Brain <vinogradov.artem@outlook.com>
2022-12-30 23:11:21 +0100

    Initial Commit

---
+++ CatsBag/CatsBagApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsBagApp.swift
+//  CatsBag
+//
+//  Created by Artem Vinogradov on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatsBagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
